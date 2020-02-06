eb6f7fe43e408686bfd7971bdbb23e25c00dee25
Achmat Fauzi <achmatfauzi86@gmail.com>
2020-02-06 07:57:56 +0700

    Label now can show grid with round numbers and cube rescale

--- Assets/Script/CubeEditor.cs
+++ Assets/Script/CubeEditor.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
+[SelectionBase]
 public class CubeEditor : MonoBehaviour
 {
     [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
+
+    TextMesh textMesh;
 
     void Update()
     {
         Vector3 snapPos;
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = (snapPos.x/gridSize) + ", " + (snapPos.z/gridSize);
+
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
     }
 }
