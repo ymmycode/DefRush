1b201bfcd7d0da238139c0c18c3eeea713c5ab38
Achmat Fauzi <achmatfauzi86@gmail.com>
2020-02-07 15:22:11 +0700

    Coloring cube when exploring

--- Assets/Script/Pathfinder.cs
+++ Assets/Script/Pathfinder.cs
@@ -7,13 +7,36 @@ public class Pathfinder : MonoBehaviour
 {
     [SerializeField] Waypoint startPoint, endPoint; // for deciding start and end point
     Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>(); //dont add same coordinates
-
+    Vector2Int[] directions = { 
+        Vector2Int.up,
+        Vector2Int.right,
+        Vector2Int.down,
+        Vector2Int.left
+    };
 
     // Start is called before the first frame update
     void Start()
     {
         LoadBlocks();
         ColorStartAndPoint();
+        ExploringNeighbours();
+    }
+
+    private void ExploringNeighbours()
+    {
+        foreach (Vector2Int direction in directions)
+        {
+            Vector2Int explorationCoordinates = startPoint.GetGridPos() + direction;
+            try
+            {
+                grid[explorationCoordinates].SetTopColor(Color.blue);
+            }
+            catch 
+            {
+                //prevent dictionary key not avalaible
+                print("Empty Block"); //it's okay without this print output
+            }
+        }
     }
 
     private void ColorStartAndPoint()
