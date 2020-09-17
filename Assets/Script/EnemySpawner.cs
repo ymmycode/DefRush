d438d6e228f2d66cf9416d8a076252637d26515e
Achmat Fauzi <achmatfauzi86@gmail.com>
2020-09-18 01:53:30 +0700

    enemies spawner

---
+++ Assets/Script/EnemySpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]float secondsBetweenSpawn = 2f;
+    [SerializeField] GameObject enemyPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawningEnemies());
+    }
+
+    IEnumerator SpawningEnemies()
+    {
+        while (true)
+        {
+            Instantiate (enemyPrefab, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(secondsBetweenSpawn);
+        }
+    }
+}
